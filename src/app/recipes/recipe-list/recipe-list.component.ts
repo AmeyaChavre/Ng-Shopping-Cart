c4fb68ef087c130914bd932c817d75516bfd8054
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-30 02:24:52 +0530

    Commit#141

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,8 +10,8 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [ 
   new Recipe('Hot Dog', 'This is a recipe for Hot Dog', 
   	'https://upload.wikimedia.org/wikipedia/commons/b/b1/Coneyhdog.jpg'), 
-   new Recipe('Test Recipe', 'This is a test recipe', 
-    'https://upload.wikimedia.org/wikipedia/commons/b/b1/Coneyhdog.jpg')
+   new Recipe('Doughnut Recipe', 'This is a recipe for Doughnut', 
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Chocolate_Frosted_Donuts_%28Coffee_An%29%2C_Westport%2C_CT_06880_USA_-_Mar_2013.jpg/220px-Chocolate_Frosted_Donuts_%28Coffee_An%29%2C_Westport%2C_CT_06880_USA_-_Mar_2013.jpg')
   ];
 
   constructor() { }
