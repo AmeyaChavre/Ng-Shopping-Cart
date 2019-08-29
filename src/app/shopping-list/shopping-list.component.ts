213c747195501068bad7930ce5a6e0f96b25e5b4
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-30 03:27:25 +0530

    Commit#219

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -8,6 +8,9 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
  	ingredients: Ingredient[] = [
+ 		new Ingredient("mustard",1),
+ 		new Ingredient("wheat flour",1),
+ 		new Ingredient("garlic",1)
  	];
   constructor() { }
 
