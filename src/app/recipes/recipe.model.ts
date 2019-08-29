0c146032f9a27927d984a7128d53d55f3ea55994
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-29 17:32:28 +0530

    Commit#89

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -3,7 +3,7 @@ export class Recipe{
 	public description:string;
 	public imagePath:string;
 
-	constructor(name:string,description:string){
+	constructor(name:string,description:string,imagePath:string){
 
 	}	
 }
