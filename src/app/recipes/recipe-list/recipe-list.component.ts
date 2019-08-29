d15579b1b39761a76a7b8532564a00b25e5cd371
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-30 02:28:38 +0530

    Commit#146

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,7 +12,7 @@ export class RecipeListComponent implements OnInit {
   	'https://upload.wikimedia.org/wikipedia/commons/b/b1/Coneyhdog.jpg'), 
   new Recipe('Doughnut Recipe', 'This is a recipe for Doughnut', 
     'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Chocolate_Frosted_Donuts_%28Coffee_An%29%2C_Westport%2C_CT_06880_USA_-_Mar_2013.jpg/220px-Chocolate_Frosted_Donuts_%28Coffee_An%29%2C_Westport%2C_CT_06880_USA_-_Mar_2013.jpg'),
-  new Recipe('Garlic Bread', 'This is a recipe for Hot Dog', 
+  new Recipe('Garlic Bread', 'This is a recipe for Garlic Bread', 
     'https://upload.wikimedia.org/wikipedia/commons/b/b1/Coneyhdog.jpg'), 
 
   ];
