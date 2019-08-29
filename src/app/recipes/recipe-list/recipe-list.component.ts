945e9450d86f1e315dd3e36a1c2e9351ea4ff9da
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-29 17:56:25 +0530

    Commit#101

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [ 
-  new Recipe('', '', '') 
+  new Recipe('Test Recipe', 'This is a test recipe', 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjX5rXQiajkAhUPdysKHZy3CYcQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.myrecipes.com%2F&psig=AOvVaw2ZdzpXvyVidFBOPdmnxiEx&ust=1567167826861840') 
   ];
 
   constructor() { }
