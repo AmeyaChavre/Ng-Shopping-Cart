bd99332fe3864eeea6cbf3393738db5499480123
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-30 02:18:01 +0530

    Commit#136

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [ 
   new Recipe('Test Recipe', 'This is a test recipe', 
-  	'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjX5rXQiajkAhUPdysKHZy3CYcQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.myrecipes.com%2F&psig=AOvVaw2ZdzpXvyVidFBOPdmnxiEx&ust=1567167826861840') 
+  	'https://upload.wikimedia.org/wikipedia/commons/b/b1/Coneyhdog.jpg') 
   ];
 
   constructor() { }
