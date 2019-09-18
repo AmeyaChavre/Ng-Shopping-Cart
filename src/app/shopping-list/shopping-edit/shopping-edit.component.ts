7334360e8dab22d832ea1682a3bff892a8e5524b
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-18 22:36:10 +0530

    Commit#361

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
