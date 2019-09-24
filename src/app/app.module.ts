02b0bb29da0e5dba16fb73dce2a6e49e9d643a58
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-24 20:02:04 +0530

    Commit#384

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-
+import { DropdownDirective } from './shared/dropdown.directive';
 
 
 
