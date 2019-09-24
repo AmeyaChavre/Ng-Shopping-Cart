12752207430b5bef512659d9c2b800c95870dcae
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-24 19:54:52 +0530

    Commit#379

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { PaymentsModComponent } from './payments-mod/payments-mod.component';
-import { InventoryModComponent } from './inventory-mod/inventory-mod.component';
+
 
 
 
