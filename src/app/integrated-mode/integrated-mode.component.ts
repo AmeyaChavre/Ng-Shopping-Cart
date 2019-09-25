b11a8a5f65cfa1cfc91d8ed6de4dc3e3ad59ae76
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-25 12:48:39 +0530

    Commit#396

---
+++ src/app/integrated-mode/integrated-mode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-integrated-mode',
+  templateUrl: './integrated-mode.component.html',
+  styleUrls: ['./integrated-mode.component.css']
+})
+export class IntegratedModeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
