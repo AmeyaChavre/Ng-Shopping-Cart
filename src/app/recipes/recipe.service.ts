f09492e4bc0119282ad4d3f899f1269fdde21d9d
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-08 00:03:25 +0530

    Commit#401

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,15 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService {
+
+recipes: Recipe[] = [ 
+  new Recipe('Hot Dog', 'This is a recipe for Hot Dog', 
+  	'https://upload.wikimedia.org/wikipedia/commons/b/b1/Coneyhdog.jpg'), 
+  new Recipe('Doughnut', 'This is a recipe for Doughnut', 
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Chocolate_Frosted_Donuts_%28Coffee_An%29%2C_Westport%2C_CT_06880_USA_-_Mar_2013.jpg/220px-Chocolate_Frosted_Donuts_%28Coffee_An%29%2C_Westport%2C_CT_06880_USA_-_Mar_2013.jpg'),
+  new Recipe('Garlic Bread', 'This is a recipe for Garlic Bread', 
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Garlicbread.jpg/235px-Garlicbread.jpg'), 
+
+  ];
 
 }
