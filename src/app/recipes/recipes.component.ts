55da37bdbc71879789ce5fffcc0398f1cbb23e9c
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-10 12:54:36 +0530

    Commit#426

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -17,7 +17,9 @@ export class RecipesComponent implements OnInit {
   ngOnInit() {
   	this.recipeService.recipeSelected
   	.subscribe(
-  		(recipe: Recipe));
+  		(recipe: Recipe)=> {
+          this.selectedRecipe = recipe;
+        });
   		
   }
 
