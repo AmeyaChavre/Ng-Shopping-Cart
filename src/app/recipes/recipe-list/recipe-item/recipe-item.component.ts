0790fc97886e99dfb64b8350024f8333b6b56c8b
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-10 12:29:54 +0530

    Commit#415

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -13,7 +14,7 @@ export class RecipeItemComponent implements OnInit {
   
 
 
-  constructor() { }
+  constructor(private recipeService : RecipeService) { }
 
   ngOnInit() {
   }
