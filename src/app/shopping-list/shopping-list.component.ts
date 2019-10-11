317bc5f098d5c297120cf171efba6aca8e12f0a2
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-11 20:00:26 +0530

    Commit#439

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from '';
+import { ShoppingListService } from './shopping-list.service';
 
 
 @Component({
