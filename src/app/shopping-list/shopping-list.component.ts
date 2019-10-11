bc276f538917c9ea3cf14d4f0dc5245dd01a63cb
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-11 20:38:18 +0530

    Commit#453

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -17,7 +17,9 @@ export class ShoppingListComponent implements OnInit {
   	this.ingredients = this.slService.getIngredients();
   	this.slService.ingredientsChanged
   	.subscribe(
-
+  		(ingredients: Ingredient[]) => {
+  			this.ingredients = ingredients;
+  		}
   	);
   }
 
