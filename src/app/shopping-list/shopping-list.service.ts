f4b1adb42f86fa486e28e424854b21ec1e7f7b8a
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-11 19:52:10 +0530

    Commit#431

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,7 +1,7 @@
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
- 	ingredients: Ingredient[] = [
+ private ingredients: Ingredient[] = [
  		new Ingredient("mustard",1),
  		new Ingredient("wheat flour",1),
  		new Ingredient("garlic",1)
