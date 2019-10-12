e2a38203376d5078b424cb9bc0852f8fe31c9e37
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-12 23:15:24 +0530

    Commit#461

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,11 +7,11 @@ recipeSelected = new EventEmitter<Recipe>();
 
 private recipes: Recipe[] = [ 
   new Recipe('Hot Dog', 'This is a recipe for Hot Dog', 
-  	'https://upload.wikimedia.org/wikipedia/commons/b/b1/Coneyhdog.jpg'), 
+  	'https://upload.wikimedia.org/wikipedia/commons/b/b1/Coneyhdog.jpg',[]), 
   new Recipe('Doughnut', 'This is a recipe for Doughnut', 
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Chocolate_Frosted_Donuts_%28Coffee_An%29%2C_Westport%2C_CT_06880_USA_-_Mar_2013.jpg/220px-Chocolate_Frosted_Donuts_%28Coffee_An%29%2C_Westport%2C_CT_06880_USA_-_Mar_2013.jpg'),
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Chocolate_Frosted_Donuts_%28Coffee_An%29%2C_Westport%2C_CT_06880_USA_-_Mar_2013.jpg/220px-Chocolate_Frosted_Donuts_%28Coffee_An%29%2C_Westport%2C_CT_06880_USA_-_Mar_2013.jpg',[]),
   new Recipe('Garlic Bread', 'This is a recipe for Garlic Bread', 
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Garlicbread.jpg/235px-Garlicbread.jpg'), 
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Garlicbread.jpg/235px-Garlicbread.jpg',[]), 
 
   ];
 
