96194f6d455f745fb8f3b8f4bd20dd48e7102ddd
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-13 15:28:58 +0530

    Commit#485

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -19,8 +19,9 @@ export class ShoppingListService {
  	}
 
  	addIngredients(ingredients: Ingredient[]){
- 		for(let ingredient of ingredients){
- 			this.addIngredient(ingredient);
- 		}
+ 	//	for(let ingredient of ingredients){
+ 	//		this.addIngredient(ingredient);
+ 	//	}
+ 		this.ingredients.push(...ingredients);
  	}
 }
