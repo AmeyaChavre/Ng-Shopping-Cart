381dcea263c0e246868d731d2f6b9c4f7e1b5493
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-14 20:38:24 +0530

    Commit#513

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes } from '@angular/routes';
 import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from '';
 
 const appRoutes: Routes = [
 { path: 'recipes', component: RecipesComponent },
