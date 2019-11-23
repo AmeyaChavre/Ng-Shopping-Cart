30b749cd3d44af92e06396f171e7c73e6c22fe52
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-23 12:18:46 +0530

    Commit#587

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { DeleteIngredientsComponent } from './delete-ingredients/delete-ingredients.component';
+
 
 
 @NgModule({
