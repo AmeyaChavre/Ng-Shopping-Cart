a7ad2ebedbfbe033efe2361c773c1ef55ee999b4
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-25 06:46:53 +0530

    Commit#605

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -13,7 +13,7 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService : RecipeService, private route : ActivatedRoute) { 
+  constructor(private recipeService : RecipeService, private route : ActivatedRoute, private router: Router) { 
   }
 
   ngOnInit() {
@@ -31,6 +31,6 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   onEditRecipe(){
-    
+
   }
 }
