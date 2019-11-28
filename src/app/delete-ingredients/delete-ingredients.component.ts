6985cd96a0780e95bedf3b7739fa8227493c12fa
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-28 20:57:56 +0530

    Commit#618

---
+++ src/app/delete-ingredients/delete-ingredients.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-ingredients',
+  templateUrl: './delete-ingredients.component.html',
+  styleUrls: ['./delete-ingredients.component.css']
+})
+export class DeleteIngredientsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
