321dfd4d05a73c9d45df3472f67b1387f530d9e3
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-30 21:06:02 +0530

    Commit#629

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,8 +30,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     ShoppingEditComponent,
     DropdownDirective,
     RecipeStartComponent,
-    RecipeEditComponent,
-    AddIngredientsComponent
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
