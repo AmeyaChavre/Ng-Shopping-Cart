0fc6e9a63cab975641a39a5e9313267cc208260d
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-30 21:05:22 +0530

    Commit#626

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,6 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { AddIngredientsComponent } from './add-ingredients/add-ingredients.component';
 
 
 
